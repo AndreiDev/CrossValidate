a7140ef73b5c7802ad64fb01abc65fa945d42536
AndreiDev <andrei.averkin@gmail.com>
2013-08-24 21:47:11 +0300

    Done Ajax for step 2

--- CVapp/static/js/main.js
+++ CVapp/static/js/main.js
@@ -10,11 +10,18 @@ $(document).ready(function () {
 	var sub2FollowersN = 0;
 	var outputUsername1_cross = "both";
 	var outputUsername2_cross = "both";
+	var outputUsername1_crossFollowing = 0;
+	var outputUsername1_crossFollowers = 0;	
+	var outputUsername2_crossFollowing = 0;
+	var outputUsername2_crossFollowers = 0;	
+	var NumOfFetches = 0
 
 	$('#outputUsername1_message').hide();
 	$('#outputUsername1').hide();	
 	$('#outputUsername2_message').hide();
 	$('#outputUsername2').hide();	
+	$('#toStep2').hide();
+	$('#toStep2_message').hide();
 	
 	function getUser1Stats(data){
 		if (data.userFollowing){
@@ -31,17 +38,25 @@ $(document).ready(function () {
 	    	sub1FollowingN = Math.ceil(data.following / 5000);
 	    	sub1FollowersN = Math.ceil(data.followers / 5000);	    
 	    	sub1Done = true;
+	    	if (sub2Done == true) {
+				$('#toStep2').show();
+				$('#toStep2_message').show();	
+	    	}	    		    	
 	   	}
 	   	else if (data.message) {
 	   		$('#outputUsername1').hide();	
 	   		$('#outputUsername1_message').show();
 	   		$('#outputUsername1_message').text(data.message);
 	   		sub1Done = false;
+			$('#toStep2').hide();
+			$('#toStep2_message').hide();	   		
 	   	}
 	    else{
 	    	$('#outputUsername1').hide();	
 	    	$('#outputUsername1_message').hide();
 			sub1Done = false;
+			$('#toStep2').hide();
+			$('#toStep2_message').hide();			
 		}
 	}
 	
@@ -60,17 +75,25 @@ $(document).ready(function () {
 	    	sub2FollowingN = Math.ceil(data.following / 5000);
 	    	sub2FollowersN = Math.ceil(data.followers / 5000);	    
 	    	sub2Done = true;
+	    	if (sub1Done == true) {
+				$('#toStep2').show();
+				$('#toStep2_message').show();	
+	    	}    	
 	   	}
 	   	else if (data.message) {
 	   		$('#outputUsername2').hide();	
 	   		$('#outputUsername2_message').show();
 	   		$('#outputUsername2_message').text(data.message);
 	   		sub2Done = false;
+			$('#toStep2').hide();
+			$('#toStep2_message').hide();	   		
 	   	}
 	    else{
 	    	$('#outputUsername2').hide();	
 	    	$('#outputUsername2_message').hide();
 			sub2Done = false;
+			$('#toStep2').hide();
+			$('#toStep2_message').hide();			
 		}	    	
 	}	
 	//$('#outputUsername1_cross').find(":selected").text()
@@ -104,6 +127,52 @@ $(document).ready(function () {
 		}
 	});	
 	
+	$('#toStep2').click(function(){	
+		
+		if (outputUsername1_cross=="both") {
+			outputUsername1_crossFollowing = 1;
+			outputUsername1_crossFollowers = 1;			
+		}
+		else if (outputUsername1_cross=="following"){
+			outputUsername1_crossFollowing = 1;
+			outputUsername1_crossFollowers = 0;					
+		}
+		else if (outputUsername1_cross=="followers"){
+			outputUsername1_crossFollowing = 0;
+			outputUsername1_crossFollowers = 1;					
+		}	
+		if (outputUsername2_cross=="both") {
+			outputUsername2_crossFollowing = 1;
+			outputUsername2_crossFollowers = 1;			
+		}
+		else if (outputUsername2_cross=="following"){
+			outputUsername2_crossFollowing = 1;
+			outputUsername2_crossFollowers = 0;					
+		}
+		else if (outputUsername2_cross=="followers"){
+			outputUsername2_crossFollowing = 0;
+			outputUsername2_crossFollowers = 1;					
+		}	
+		NumOfFetches = userFollowingN+userFollowersN+sub1FollowingN*outputUsername1_crossFollowing+sub1FollowersN*outputUsername1_crossFollowers+sub2FollowingN*outputUsername2_crossFollowing+sub2FollowersN*outputUsername2_crossFollowers;
+		if (NumOfFetches <= 15) {
+			$('#inputUsername2').prop('disabled', true);
+			$('#inputUsername1').prop('disabled', true);
+			$('input[name=outputUsername1_cross]:radio').prop('disabled', true);
+			$('input[name=outputUsername2_cross]:radio').prop('disabled', true);
+			$('#subject1go').hide();
+			$('#subject2go').hide();
+			$('#toStep2').hide();			
+			$('#toStep2_message').hide();
+			
+			
+		} 
+		else {
+			$('#toStep2').show();
+			$('#toStep2_message').text('Number of fetches exceeded (' + NumOfFetches +')');
+			$('#toStep2_message').show();			
+		}
+	});	
+	
 	$("input:radio[name=outputUsername1_cross]").click(function() {
     	outputUsername1_cross = $(this).val();
 	});
@@ -111,10 +180,6 @@ $(document).ready(function () {
 	$("input:radio[name=outputUsername2_cross]").click(function() {
     	outputUsername2_cross = $(this).val();
 	});	
-	
-	$('#pleaseWait').submit(function() {
-    	window.setTimeout('location.reload()', 500);
-  	});
     
 	// media query event handler
 	if (matchMedia) {
